f237357d2cc89133a0bb9fc242c3b6427e02d532
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-13 17:52:55 -0400

    fixed spelling error

--- book.rb
+++ book.rb
@@ -1,25 +1,30 @@
 class Book
-    def intialize(title, author)
+    def initialize(title, author)
         @title = title
         @author = author
         @finished = false
         @count = 3
     end
+
     def title=(str)
         @title = str
     end
+
     def title
         @title
     end
+
     def author=(str)
         @author = str
     end
     def author
         @author
     end
+
     def finished
         @finished
     end
+
     def count
         @count
     end

14914a0449908304a17fd243dc59c4ea89a11b21
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-13 18:26:04 -0400

    added recommended books method

--- book.rb
+++ book.rb
@@ -8,4 +8,12 @@ class Book
         @finished = false
         @count = 3
     end
+
+    def recommended_books
+        [
+            Book.new("The Well-Grounded Rubyist", "David A. Black"),
+            Book.new("Practical Object-Oriented Design in Ruby", "sandi Metz"),
+            Book.new("Effective testing with RSpec 3", "Myron Marston")
+        ]
+    end
 end

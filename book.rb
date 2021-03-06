820485745a413421dc21ab3f018d6cc4c5cb338a
ByronHolmes-at-129020800614 <lshopeful2018@gmail.com>
2021-03-06 08:22:37 -0600

    Added method recommended_books to Book class in book.rb

--- book.rb
+++ book.rb
@@ -16,6 +16,15 @@ class Book
     def author
         @author
     end
+
+    def recommended_books(*books)
+        book_array = [
+            Book.new("The Well-Grounded Rubyist", "David A. Black"),
+            Book.new("Practical Object-Oriented Design in Ruby", "Sandi Metz"),
+            Book.new("Effective Testing with RSpec 3", "Myron Marston"),
+          ]
+    end
+
 end
 
 
